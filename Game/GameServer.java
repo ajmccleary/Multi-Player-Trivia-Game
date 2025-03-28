9620bf958e973e135e408c7ab5fd3688cf219848
Zakaria Merzougui <zakariamer123@gmail.com>
2025-03-27 22:57:16 -0400

    added questions reading from config file and adding answers

--- Game/GameServer.java
+++ Game/GameServer.java
@@ -3,7 +3,9 @@ package game;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.*;
 
@@ -19,8 +21,9 @@ public class GameServer {
     //instance variables
     private int questionNumber = 0;
 
+
     //constructor method
-    public GameServer () {
+    public GameServer() {
         //declare inFile
         inFile = new File("ipConfig.txt");
         
@@ -76,6 +79,54 @@ public class GameServer {
 
     //main
     public static void main (String args[]) {
+        System.out.println("Game Server Starting...");
+
+        // Getting size of config file
+        File configFile = new File("questions.txt");
+        int size = 0;
+        try (Scanner fileScan = new Scanner(configFile)) {
+            while (fileScan.hasNextLine()) {
+                size++;
+                fileScan.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("questions.txt not found in root directory. Exiting program");
+            System.exit(1);
+        }
+
+        // Create an array to hold the questions
+        String[] questions = new String[size]; 
+        // String[] options = new String[4];
+
+        List<String> options = new ArrayList<>();
+
+        int index = 0;
+        File questionFile = new File("questions.txt");
+        try (Scanner fileScan = new Scanner(questionFile)) {
+            while (fileScan.hasNextLine()) {
+                String[] parts = fileScan.nextLine().split("\\|");
+
+                questions[index] = parts[0];
+                for (int i = 1; i <= 4; i++) {
+                    // options[i - 1] = parts[i];
+
+                    options.add(parts[i]);
+                }
+                index++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("questions.txt not found in root directory. Exiting program");
+            System.exit(1);
+        }
+
+        for (int i = 0; i < questions.length; i++) {
+            System.out.println("Question " + (i + 1) + ": " + questions[i]);
+            for (int j = 0; j < 4; j++) {
+                System.out.println("Option " + (j + 1) + ": " + options.get(j + (i * 4)));
+            }
+        }
+
+
         GameServer gs = new GameServer();
 
         executorService = Executors.newFixedThreadPool(10);
@@ -88,7 +139,5 @@ public class GameServer {
 
             gs.questionNumber++;
         }
-
-        //trip shutdown flag
     }
 }
