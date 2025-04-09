ac497c4bafe5267f10ff8ec35de4386621a72cfc
Zakaria <zakariamer123@gmail.com>
2025-04-08 23:12:39 -0400

    reads file for server ip

--- Game/GameClient.java
+++ Game/GameClient.java
@@ -1,13 +1,29 @@
 package Game;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Scanner;
+
 public class GameClient {
     //instance variables
     private ClientWindow window;
 
     //constructor method
     public GameClient () {
+        File file = new File("ipConfig.txt");
+        Scanner scan = null;
+        try {
+            scan = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        String ipPort = scan.nextLine();
+        String ip = ipPort.split(" ")[0];
+        System.out.println(ip);
         //initialize game window
-        this.window = new ClientWindow("127.0.0.1", 8001);
+        this.window = new ClientWindow(ip, 8001);
     }
 
     public static void main (String args[]) {
